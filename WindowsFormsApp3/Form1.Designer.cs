c4520c139426ba8c8ffbd498dd62a60693badfe6
«Evgenia» <ekadolo@mail.ru>
2022-09-06 21:16:18 +0500

    api

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(86, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(426, 59);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(302, 308);
+            this.textBox1.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Arabic",
+            "Bulgarian",
+            "Chinese(Simplified)",
+            "Chinese(Traditional)",
+            "Croatian",
+            "Czech",
+            "Danish",
+            "Dutch",
+            "English",
+            "Finnish",
+            "French",
+            "German",
+            "Greek",
+            "Hungarian",
+            "Korean",
+            "Italian",
+            "Japanese",
+            "Polish",
+            "Portuguese",
+            "Russian",
+            "Slovenian",
+            "Spanish",
+            "Swedish",
+            "Turkish"});
+            this.comboBox1.Location = new System.Drawing.Point(535, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(146, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(320, 187);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 30);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Recognize";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 59);
+            this.pictureBox1.MaximumSize = new System.Drawing.Size(302, 308);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(302, 308);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(457, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Language";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 379);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Parse Image";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        string filePath;
+        string language;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            if(filePath != null)
+            {
+                var fileBytes = File.ReadAllBytes(filePath);
+                var fileName = Path.GetFileName(filePath);
+
+                var requestContent = new MultipartFormDataContent();
+
+                requestContent.Add(new StringContent("c848ee14b288957"), "apikey");
+                if (language != null)
+                    requestContent.Add(new StringContent(language), "language");
+
+                var imageContent = new ByteArrayContent(fileBytes);
+                imageContent.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
+                requestContent.Add(imageContent, "file", fileName);
+
+                var client = new HttpClient();
+                var response = await client.PostAsync("https://api.ocr.space/parse/image", requestContent);
+
+                HttpContent responseContent = response.Content;
+
+                using (var reader = new StreamReader(await responseContent.ReadAsStreamAsync()))
+                {
+                    string result = await reader.ReadToEndAsync();
+                    Info info = JsonConvert.DeserializeObject<Info>(result);
+                    if (info.OCRExitCode == 1)
+                        textBox1.Text = info.ParsedResults[0].ParsedText;
+                    else
+                    {
+                        for (int i = 0; i < info.ErrorMessage.Length; i++)
+                        {
+                            textBox1.Text = info.ErrorMessage[i];
+                        }
+                    }
+                }
+            }
+            else
+            {
+                textBox1.Text = "Изображение не выбрано";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap image;
+
+            OpenFileDialog open_dialog = new OpenFileDialog();
+            open_dialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG";
+            if (open_dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    image = new Bitmap(open_dialog.FileName);
+                    filePath = open_dialog.FileName;
+                    this.pictureBox1.Size = image.Size;
+                    pictureBox1.Image = image;
+                    pictureBox1.Invalidate();
+                }
+                catch
+                {
+                    DialogResult result = MessageBox.Show("Невозможно открыть выбранный файл",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex > -1)
+            {
+                switch(comboBox1.SelectedIndex)
+                {
+                    case 0:
+                        language = "ara";
+                        break;
+                    case 1:
+                        language = "bul";
+                        break;
+                    case 2:
+                        language = "chs";
+                        break;
+                    case 3:
+                        language = "cht";
+                        break;
+                    case 4:
+                        language = "hrv";
+                        break;
+                    case 5:
+                        language = "cze";
+                        break;
+                    case 6:
+                        language = "dan";
+                        break;
+                    case 7:
+                        language = "dut";
+                        break;
+                    case 8:
+                        language = "eng";
+                        break;
+                    case 9:
+                        language = "fin";
+                        break;
+                    case 10:
+                        language = "fre";
+                        break;
+                    case 11:
+                        language = "ger";
+                        break;
+                    case 12:
+                        language = "gre";
+                        break;
+                    case 13:
+                        language = "hun";
+                        break;
+                    case 14:
+                        language = "kor";
+                        break;
+                    case 15:
+                        language = "ita";
+                        break;
+                    case 16:
+                        language = "jpn";
+                        break;
+                    case 17:
+                        language = "pol";
+                        break;
+                    case 18:
+                        language = "por";
+                        break;
+                    case 19:
+                        language = "rus";
+                        break;
+                    case 20:
+                        language = "slv";
+                        break;
+                    case 21:
+                        language = "spa";
+                        break;
+                    case 22:
+                        language = "swe";
+                        break;
+                    case 23:
+                        language = "tur";
+                        break;
+                }
+            }
+        }
+    }
+
+    class Info
+    {
+        public ParsedResults[] ParsedResults { get; set; }
+        public int OCRExitCode { get; set; }
+        public string[] ErrorMessage { get; set; }
+    }
+
+    class ParsedResults
+    {
+        public string ParsedText { get; set; }
+    }
+}
